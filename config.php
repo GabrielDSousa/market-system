d4fdd4281e6ee51b9748e2af46fc6323b5136355
GabrielDSousa <gabrielramos.email@gmail.com>
2023-04-01 22:10:35 -0300

    feat: Adiciona conexão ao banco de dados
    Adiciona as variáveis de ambiente para a conexão com o Postgresql no arquivo config.php e cria a classe Database para criar uma conexão com o banco de dados, além de adicionar o dump do database.
    
    Closes #1

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'database' => [
+        'driver' => 'pgsql',
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'market',
+        'user' => 'postgres',
+        'password' => 'postgres'
+    ]
+];
